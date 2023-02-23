93e1be4bfd6d30fe56c16c3d43036dc8536efc07
Asia Rusnak <joannarusnak8383@gmail.com>
2023-02-23 19:26:36 +0100

    react app with errors

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import "./bootstrap/bootstrap.min.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+	<StrictMode>
+		{" "}
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
+);
